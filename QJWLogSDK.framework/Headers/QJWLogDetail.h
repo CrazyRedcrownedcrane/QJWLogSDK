baa27771291fff35ae2e5d1311631f23bae8745f
qianjinwu <605532855@qq.com>
2019-08-15 17:25:41 +0800

    添加spec/添加sdk

---
+++ QJWLogSDK.framework/Headers/QJWLogDetail.h
@@ -0,0 +1,17 @@
+//
+//  QJWLogDetail.h
+//  QJWLogSDK
+//
+//  Created by KevinChien on 2019/8/12.
+//  Copyright © 2019 Kevin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QJWLogDetail : NSObject
++ (void)printFuncLog;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QJWLogSDK.framework/Headers/QJWLogSDK.h
@@ -0,0 +1,19 @@
+//
+//  QJWLogSDK.h
+//  QJWLogSDK
+//
+//  Created by KevinChien on 2019/8/12.
+//  Copyright © 2019 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QJWLogSDK.
+FOUNDATION_EXPORT double QJWLogSDKVersionNumber;
+
+//! Project version string for QJWLogSDK.
+FOUNDATION_EXPORT const unsigned char QJWLogSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QJWLogSDK/PublicHeader.h>
+
+#import <QJWLogDetail.h>
